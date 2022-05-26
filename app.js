992e7d6010a284e54615b7ab4070b6f3214147d6
jimish1806 <jimish957@gmail.com>
2022-05-27 01:28:09 +0530

    wrote JS to trigger different input fields and toggle their appearances

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
 console.log("Jay Shree Ram");
+
+// --------------------------> Grab Elements <---------------------------- //
+let get = document.getElementById("get");
+let post = document.getElementById("post");
+let jsonBtn = document.getElementById("json");
+let constomizeParams = document.getElementById("constomizeParams");
+let submitParameter = document.getElementById("submitParameter");
+
+
+// --------------> Trigger different fields according to selection <-------------------- //
+if(get.checked)
+{
+    document.getElementById("postDetailContainer").style.display = `none`;
+}
+
+get.addEventListener("click",()=>
+{
+    document.getElementById("postDetailContainer").style.display = `none`;
+})
+
+post.addEventListener("click",()=>
+{
+    document.getElementById("postDetailContainer").style.display = `block`;
+})
+
+if(jsonBtn.checked)
+{
+    document.getElementById("parameters").style.display = `none`;
+}
+
+jsonBtn.addEventListener("click",()=>
+{
+    document.getElementById("parameters").style.display = `none`;
+    document.getElementById("requestJSON").style.display = `flex`;
+})
+
+constomizeParams.addEventListener("click",()=>
+{
+    document.getElementById("requestJSON").style.display = `none`;
+    document.getElementById("parameters").style.display = `flex`;
+})
+
+let parameterCount = 2;
+
+submitParameter.addEventListener("click",function()
+{
+    
+})
